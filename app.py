1bbb090aa050f10abbcc97906b67f556a555f096
Sugam Mehta <66478859+sugam0301@users.noreply.github.com>
2022-07-10 15:41:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,111 @@
+# from flask import Flask, request, render_template
+# from flask import Response
+# import os
+# from flask_cors import CORS, cross_origin
+# from prediction_Validation_Insertion import pred_validation
+# from trainingModel import trainModel
+# from training_Validation_Insertion import train_validation
+# # import flask_monitoringdashboard as dashboard
+# from predictFromModel import prediction
+# import config
+
+# from wsgiref import simple_server
+
+
+# os.putenv('LANG', 'en_US.UTF-8')
+# os.putenv('LC_ALL', 'en_US.UTF-8')
+
+# app = Flask(__name__)
+# # dashboard.bind(app)
+# CORS(app)
+
+
+# @app.route("/", methods=['GET'])
+# @cross_origin()
+# def home():
+#     return render_template('index.html')
+
+
+# @app.route("/predict", methods=['POST'])
+# @cross_origin()
+# def predictRouteClient():
+#     try:
+#         if request.json is not None:
+#             path = request.json['filepath']
+
+#             pred_val = pred_validation(path)  # object initialization
+
+#             pred_val.prediction_validation()  # calling the prediction_validation function
+
+#             pred = prediction(path)  # object initialization
+
+#             # predicting for dataset present in database
+#             path = pred.predictionFromModel()
+#             return Response("Prediction File created at %s!!!" % path)
+#         elif request.form is not None:
+#             path = request.form['filepath']
+
+#             pred_val = pred_validation(path)  # object initialization
+
+#             pred_val.prediction_validation()  # calling the prediction_validation function
+
+#             pred = prediction(path)  # object initialization
+
+#             # predicting for dataset present in database
+#             path = pred.predictionFromModel()
+#             return Response("Prediction File created at %s!!!" % path)
+
+#     except ValueError:
+#         return Response("Error Occurred! %s" % ValueError)
+#     except KeyError:
+#         return Response("Error Occurred! %s" % KeyError)
+#     except Exception as e:
+#         return Response("Error Occurred! %s" % e)
+
+
+# @app.route("/train", methods=['POST'])
+# @cross_origin()
+# def trainRouteClient():
+#     try:
+#         if request.json['filepath'] is not None:
+#             path = request.json['filepath']
+#             train_valObj = train_validation(path)  # object initialization
+
+#             train_valObj.train_validation()  # calling the training_validation function
+
+#             trainModelObj = trainModel()  # object initialization
+#             trainModelObj.trainingModel()  # training the model for the files in the table
+
+#     except ValueError:
+
+#         return Response("Error Occurred! %s" % ValueError)
+
+#     except KeyError:
+
+#         return Response("Error Occurred! %s" % KeyError)
+
+#     except Exception as e:
+
+#         return Response("Error Occurred! %s" % e)
+#     return Response("Training successfull!!")
+
+
+# # port = int(os.getenv("PORT", 5001))
+# # if __name__ == "__main__":
+# #     app.run(port=port, debug=True)
+
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=config.PORT)
+
+# # if __name__ == "__main__":
+# #     port = int(os.getenv("PORT"))
+# #     #clApp = ClientApp()
+# #     host = '0.0.0.0'
+# #     httpd = simple_server.make_server(host=host,port=port, app=app)
+# #     httpd.serve_forever()
+
+
+
 from flask import Flask, request, render_template
 from flask import Response
 import os
@@ -5,26 +113,21 @@ from flask_cors import CORS, cross_origin
 from prediction_Validation_Insertion import pred_validation
 from trainingModel import trainModel
 from training_Validation_Insertion import train_validation
-# import flask_monitoringdashboard as dashboard
+#import flask_monitoringdashboard as dashboard
 from predictFromModel import prediction
 import config
-
-from wsgiref import simple_server
-
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
-# dashboard.bind(app)
+#dashboard.bind(app)
 CORS(app)
-
 
 @app.route("/", methods=['GET'])
 @cross_origin()
 def home():
     return render_template('index.html')
-
 
 @app.route("/predict", methods=['POST'])
 @cross_origin()
@@ -33,11 +136,11 @@ def predictRouteClient():
         if request.json is not None:
             path = request.json['filepath']
 
-            pred_val = pred_validation(path)  # object initialization
+            pred_val = pred_validation(path) #object initialization
 
-            pred_val.prediction_validation()  # calling the prediction_validation function
+            pred_val.prediction_validation() #calling the prediction_validation function
 
-            pred = prediction(path)  # object initialization
+            pred = prediction(path) #object initialization
 
             # predicting for dataset present in database
             path = pred.predictionFromModel()
@@ -45,36 +148,40 @@ def predictRouteClient():
         elif request.form is not None:
             path = request.form['filepath']
 
-            pred_val = pred_validation(path)  # object initialization
+            pred_val = pred_validation(path) #object initialization
 
-            pred_val.prediction_validation()  # calling the prediction_validation function
+            pred_val.prediction_validation() #calling the prediction_validation function
 
-            pred = prediction(path)  # object initialization
+            pred = prediction(path) #object initialization
 
             # predicting for dataset present in database
             path = pred.predictionFromModel()
             return Response("Prediction File created at %s!!!" % path)
 
     except ValueError:
-        return Response("Error Occurred! %s" % ValueError)
+        return Response("Error Occurred! %s" %ValueError)
     except KeyError:
-        return Response("Error Occurred! %s" % KeyError)
+        return Response("Error Occurred! %s" %KeyError)
     except Exception as e:
-        return Response("Error Occurred! %s" % e)
+        return Response("Error Occurred! %s" %e)
+
 
 
 @app.route("/train", methods=['POST'])
 @cross_origin()
 def trainRouteClient():
+
     try:
         if request.json['filepath'] is not None:
             path = request.json['filepath']
-            train_valObj = train_validation(path)  # object initialization
+            train_valObj = train_validation(path) #object initialization
 
-            train_valObj.train_validation()  # calling the training_validation function
+            train_valObj.train_validation()#calling the training_validation function
 
-            trainModelObj = trainModel()  # object initialization
-            trainModelObj.trainingModel()  # training the model for the files in the table
+
+            trainModelObj = trainModel() #object initialization
+            trainModelObj.trainingModel() #training the model for the files in the table
+
 
     except ValueError:
 
@@ -89,13 +196,20 @@ def trainRouteClient():
         return Response("Error Occurred! %s" % e)
     return Response("Training successfull!!")
 
+port = int(os.getenv("PORT",5001))
+if __name__ == "__main__":
+    port = 5001
+    app.run(port=port, debug=True)
+
+
+
 
 # port = int(os.getenv("PORT", 5001))
 # if __name__ == "__main__":
 #     app.run(port=port, debug=True)
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=config.PORT)
+# if __name__ == '__main__':
+#     app.run(host="0.0.0.0", port=config.PORT)
 
 # if __name__ == "__main__":
 #     port = int(os.getenv("PORT"))
